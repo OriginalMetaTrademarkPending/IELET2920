528e722a4d6f973cff94d2dfcb989ff94e4bb029
OriginalMetaTrademarkPending <metaadmir@gmail.com>
2023-03-24 17:04:25 +0100

    Added MATLAB code for system identification

---
+++ MATLAB_sys_identification/diff_eq.m
@@ -0,0 +1,15 @@
+function dmdt = diff_eq(~, m, theta, u)
+% The differential equations describing hand grip dynamics
+%   m -> vector consisting of active muscle mass m_a and fatigued muscle
+%   mass m_f. m_a is the first element and m_f is the second element.
+%   theta -> vector consisting of the parameters to be evaluated.
+%   theta(1) = theta_af
+%   theta(2) = theta_ar
+%   theta(3) = theta_ra
+%   theta(4) = theta_fa
+%   u -> scalar which serves as the control input for the model.
+M = 2.295;
+dmdt = [-theta(1)-theta(2) - ((theta(3) - theta(2))*m(1)) + ((theta(4) - theta(3))*m(2)) + (theta(3)*M*u);
+    (theta(1)*m(1)) + theta(4)*m(2)];
+end
+
---
+++ MATLAB_sys_identification/fit_ode_parameters.m
@@ -0,0 +1,55 @@
+% We start by importing the differential equations which describe the
+% system.
+type diff_eq
+
+
+%% TESTING THE DYNAMICS
+% The experiment shall last for 30 seconds. This is our timespan. In
+% addition, measurements will be taken every 1 ms (corresponds to 30000
+% samples/data points).
+tspan = 0:0.001:30;
+
+% The input signal is defined below as a square wave with on-time of 10 
+% seconds and off-time of 5 seconds.
+u = @(t) heaviside(t - 5) - heaviside(t - 15) + heaviside(t - 20) - heaviside(t - 30);
+
+% Somewhere here, we need the actual parameters...the least squares problem
+% requires the response from the system with real parameters (or as real 
+% as they can get)...
+% Until further notice, do some guesstimates, see how the system reacts
+theta_real = [4, -3.9, 3.5, -4];
+m0 = [0 0];
+soltrue = ode45(@(t, m)diff_eq(t, m, theta_real, u(t)), tspan, m0);
+m_true = deval(soltrue, tspan);
+y_true = m_true(1, :);
+y_hidden = m_true(2, :);
+plot(tspan, y_true);
+hold on
+plot(tspan, y_hidden);
+hold off
+
+%%
+% Next, we import the data retrieved from the system testing, as well as
+% the starting points.
+m_data = 0;
+m0 = zeros(30000, 1);
+
+% In order to find the theta-parameters, we need to declare them as
+% optimization variables.
+theta = optimvar('theta', 5);
+
+% The objective function is the sum of squares of the differences between
+% the "real" solution and the data. In order to define the objective
+% function, we need to import the function which computes the ODE with the
+% parameters
+type theta_to_ode
+
+% Now, we express this function as an optimization expression.
+fcn = fcn2optimexpr(@theta_to_ode, theta, tspan, m0);
+
+% Finally, the objective function can be defined.
+obj = sum(sum((y_data - y_true).^2));
+
+% Now, the optimalization problem
+prob = optimproblem("Objective", obj);
+
