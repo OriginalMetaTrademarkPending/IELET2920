0cd3608dbb3299908565466029ff82f4b96c18b1
EliasLoeE <eliasle@stud.nutnu.no>
2023-04-24 09:58:01 +0200

    updating

--- bachelor_esp_kalman/.pio/build/upesy_wroom/lib11c/SPI/SPI.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/upesy_wroom/lib11c/SPI/SPI.cpp.o: \
- C:/Users/lorde/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.cpp \
- C:/Users/lorde/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.h \
- C:/Users/lorde/.platformio/packages/framework-arduinoespressif32/variants/ttgo-lora32-v1/pins_arduino.h \
- C:/Users/lorde/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-spi.h
