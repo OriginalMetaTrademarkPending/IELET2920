354c7c6aaaa87c572c4f3b1ef7beb7a213f35c73
EliasLoeE <eliasle@stud.nutnu.no>
2023-04-26 13:43:48 +0200

    cleanup

--- bachelor_esp_kalman state/.pio/build/upesy_wroom/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/upesy_wroom/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/lorde/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/lorde/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
