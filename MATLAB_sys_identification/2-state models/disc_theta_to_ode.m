239388027b0ecb62c664f153ca1c228a19e87394
OriginalMetaTrademarkPending <metaadmir@gmail.com>
2023-05-12 15:55:58 +0200

    Final updates to the code

--- MATLAB_sys_identification/2-state models/disc_theta_to_ode.m
+++ MATLAB_sys_identification/2-state models/disc_theta_to_ode.m
@@ -1,4 +1,4 @@
-function solution = disc_theta_to_ode(phi, N, u, M)
+function sol = disc_theta_to_ode(phi, N, u, M)
 %The parametric differential equation to be implemented in the objective
 %function. Note that only the active muscle mass is measured, the fatigued
 %muscle mass is a hidden variable.
@@ -7,4 +7,5 @@ solution(:, 1) = zeros(2, 1);
 for i = 2:N
     solution(:, i) = disc_diff_eq(phi, solution(:, i-1), u(i-1), M);
 end
+sol = solution(1, :);
 end
