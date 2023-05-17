ccb387c0e1f30979c96e2a62d20e4a8ae4b970fa
OriginalMetaTrademarkPending <metaadmir@gmail.com>
2023-05-17 12:12:00 +0200

    Fixing rebase(hopefully)?

--- bachelor_esp_kalman/.pio/build/upesy_wroom/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/upesy_wroom/FrameworkArduino/libb64/cencode.c.o: \
- C:/Users/Elias/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- C:/Users/Elias/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
