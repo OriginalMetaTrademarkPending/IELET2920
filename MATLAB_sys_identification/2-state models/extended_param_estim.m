50c5653497554837eb8e139709230d790b1e5426
OriginalMetaTrademarkPending <metaadmir@gmail.com>
2023-06-03 17:45:17 +0200

    Solving the merge conflicts

--- MATLAB_sys_identification/2-state models/extended_param_estim.m
+++ MATLAB_sys_identification/2-state models/extended_param_estim.m
@@ -6,27 +6,9 @@ r_data = r_readings.Data1';
 R = cov(r_data);
 
 % The Q vector is only available as a tuning variable. We have chosen to
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 % start with Q = 2.5*R in the diagonal, as well as 0 in the covariance
 % elements.
 Q = 1.2*R*[1, 0; 0, 1];
-=======
-% start with Q = 1/5*R in the diagonal, as well as 0 in the covariance
-% elements.
-Q = 0.2*R*eye(2);
->>>>>>> 9f9fe91 (Implemented the first part of the estimator (Kalman Filter). Trying to find a solution for the least squares estimator)
-=======
-% start with Q = 2.5*R in the diagonal, as well as 0 in the covariance
-% elements.
-Q = 1.2*R*[1, 0; 0, 1];
->>>>>>> beb4004 (Full Kalman Filter implementation in MATLAB)
-=======
-% start with Q = 2.5*R in the diagonal, as well as 0 in the covariance
-% elements.
-Q = 1.2*R*[1, 0; 0, 1];
->>>>>>> master
 
 %% MODEL IMPORT
 % We start by importing the discretized differential equations (via Forward
@@ -36,19 +18,7 @@ type disc_diff_eq
 % Next, we import the data retrieved from the system testing, as well as
 % the starting points. For this we need the filepath where the readings
 % are.
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 FILEPATH = "../../python_scripts/test1.csv";
-=======
-FILEPATH = "../../python_scripts/test6.csv";
->>>>>>> 9f9fe91 (Implemented the first part of the estimator (Kalman Filter). Trying to find a solution for the least squares estimator)
-=======
-FILEPATH = "../../python_scripts/test1.csv";
->>>>>>> beb4004 (Full Kalman Filter implementation in MATLAB)
-=======
-FILEPATH = "../../python_scripts/test1.csv";
->>>>>>> master
 readings = readtable(FILEPATH, 'VariableNamingRule', 'preserve');
 y = readings.Data1';
 N = max(size(y));           %Number of samples to be registered
@@ -60,19 +30,7 @@ t_vec = linspace(0, tspan, N);      %Time vector for plotting and input generati
 % parameters adjusted for the sample time. These parameters must be within
 % 0 and 1. The last parameter is the total muscle mass. This parameter does
 % not need to be adjusted for the sample time.
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 phi_0 = [0.9989, 0.8874, 0.1982, 0.9992, 13.8220]; 
-=======
-phi_0 = [0.99, 0.7, 0.6, 0.9, 20]; 
->>>>>>> 9f9fe91 (Implemented the first part of the estimator (Kalman Filter). Trying to find a solution for the least squares estimator)
-=======
-phi_0 = [0.9989, 0.8874, 0.1982, 0.9992, 13.8220]; 
->>>>>>> beb4004 (Full Kalman Filter implementation in MATLAB)
-=======
-phi_0 = [0.9989, 0.8874, 0.1982, 0.9992, 13.8220]; 
->>>>>>> master
 % af, 
 
 % The input signal is defined below. The function is then run with each
@@ -109,99 +67,47 @@ m_fatig = mk(2, :);
 % ylabel("Mass (kg)")
 % title("Hand Grip Model Simulation")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 %% EXTENDED ESTIMATOR IMPLEMENTATION
-=======
-%% KALMAN FILTER IMPLEMENTATION
->>>>>>> 9f9fe91 (Implemented the first part of the estimator (Kalman Filter). Trying to find a solution for the least squares estimator)
-=======
-%% EXTENDED ESTIMATOR IMPLEMENTATION
->>>>>>> 4f56570 (Changes in the new estimator, will try to finalize tomorrow)
-=======
-%% EXTENDED ESTIMATOR IMPLEMENTATION
->>>>>>> master
 % Declaring some constants. Here, the the measurement
 % function is a constant.
 H = [1 0];
 % In order to implement the Kalman filter, we first need to declare the
 % initial state estimate and the initial state covariance. We also need to
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> master
 % preallocate the predicted state and prediction covariance, as well as the
 % residual vector
-x_hat(:, 1) = zeros(2, 1);
-P_hat = 100*eye(2);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-% preallocate the predicted state and prediction covariance.
 
-x_hat = NaN(2, N);
-=======
-% preallocate the predicted state and prediction covariance, as well as the
-% residual vector
->>>>>>> 4f56570 (Changes in the new estimator, will try to finalize tomorrow)
 x_hat(:, 1) = zeros(2, 1);
 P_hat = zeros(2, 2);
->>>>>>> 9f9fe91 (Implemented the first part of the estimator (Kalman Filter). Trying to find a solution for the least squares estimator)
-=======
->>>>>>> beb4004 (Full Kalman Filter implementation in MATLAB)
-=======
->>>>>>> master
 
 x_bar = NaN(2, N);
 P_bar = zeros(2, 2);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 z = NaN(2, N);
-=======
-z = NaN(1, N);
->>>>>>> 4f56570 (Changes in the new estimator, will try to finalize tomorrow)
-=======
-z = NaN(2, N);
->>>>>>> beb4004 (Full Kalman Filter implementation in MATLAB)
-=======
-z = NaN(2, N);
->>>>>>> master
 
 % Initializing the "optimal" parameters.
 phi_star.phi = phi_0;
 phi_star1.phi = phi_0;
 phi_star2.phi = phi_0;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 % Initializing the "optimal" parameters.
 phi_star.phi = phi_0;
->>>>>>> 9f9fe91 (Implemented the first part of the estimator (Kalman Filter). Trying to find a solution for the least squares estimator)
-=======
->>>>>>> 4f56570 (Changes in the new estimator, will try to finalize tomorrow)
-=======
->>>>>>> master
 
 % Initializing the optimization variables for the least squares estimation
 optim_phi = optimvar('phi', 5, 'LowerBound', [0, 0, 0, 0, 0]);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> master
 % Initializing the residual evaluation variable
 residual_limit = NaN(2, N);
 
 % Values for checking whether the algorithm works or not
 phi_evo = NaN(5, N);
 sumsq_evo = NaN(2, N);
+
+% Calling in the iterations
+for i = 2:N
+    [x_bar(:, i-1), P_bar] = kf_predict(x_hat(:, i-1), P_hat, phi_star.phi, u_vec(i-1), Q);
+    [x_hat(:, i), P_hat, z(:, i-1)] = kf_update(y(i), x_bar(:, i-1), P_bar, H, R);
+    residual_limit(:, i-1) = sqrt([P_hat(1, 1); P_hat(2, 2)])*5;
+    %%% WILL WRITE THE ESTIMATOR CODE TOMORROW! %%%
+end
 
 % Calling in the iterations
 for i = 2:N
@@ -235,65 +141,4 @@ figure(3)
 plot(t_vec, z(2, :))
 hold on
 plot(t_vec, residual_limit(2, :));
-<<<<<<< HEAD
 plot(t_vec, -residual_limit(2, :));
-=======
-% Initializing the optimalization expression variables
-optim_expr = optimexpr(2, N);
-
-=======
->>>>>>> 4f56570 (Changes in the new estimator, will try to finalize tomorrow)
-=======
-% Initializing the residual evaluation variable
-residual_limit = NaN(2, N);
-
->>>>>>> beb4004 (Full Kalman Filter implementation in MATLAB)
-% Values for checking whether the algorithm works or not
-phi_evo = NaN(5, N);
-sumsq_evo = NaN(2, N);
-
-% Calling in the iterations
-for i = 2:N
-    [x_bar(:, i-1), P_bar] = kf_predict(x_hat(:, i-1), P_hat, phi_star.phi, u_vec(i-1), Q);
-    [x_hat(:, i), P_hat, z(:, i-1)] = kf_update(y(i), x_bar(:, i-1), P_bar, H, R);
-    residual_limit(:, i-1) = sqrt([P_hat(1, 1); P_hat(2, 2)])*5;
-    %%% WILL WRITE THE ESTIMATOR CODE TOMORROW! %%%
-end
-
-%% PLOT ALL RESULTS
-figure(1)
-hold on
-plot(t_vec, y);
-plot(t_vec, u_vec);
-plot(t_vec, x_hat(1, :));
-plot(t_vec, x_hat(2, :));
-hold off
-legend("Measurement", "Input", "Estimated Active Mass", "Estimated Fatigued Mass");
-xlabel("Time (s)")
-ylabel("Mass (kg)")
-title("Hand Grip System Identification")
-<<<<<<< HEAD
->>>>>>> 9f9fe91 (Implemented the first part of the estimator (Kalman Filter). Trying to find a solution for the least squares estimator)
-=======
-
-<<<<<<< HEAD
-%figure(2)
-%plot(t_vec, z);
->>>>>>> 4f56570 (Changes in the new estimator, will try to finalize tomorrow)
-=======
-figure(2)
-plot(t_vec, z(1, :))
-hold on
-plot(t_vec, residual_limit(1, :));
-plot(t_vec, -residual_limit(1, :));
-hold off
-
-figure(3)
-plot(t_vec, z(2, :))
-hold on
-plot(t_vec, residual_limit(2, :));
-plot(t_vec, -residual_limit(2, :));
->>>>>>> beb4004 (Full Kalman Filter implementation in MATLAB)
-=======
-plot(t_vec, -residual_limit(2, :));
->>>>>>> master
--- MATLAB_sys_identification/2-state models/fit_disc_ode_parameters.m
+++ MATLAB_sys_identification/2-state models/fit_disc_ode_parameters.m
@@ -6,107 +6,9 @@ type disc_diff_eq
 % Next, we import the data retrieved from the system testing, as well as
 % the starting points. For this we need the filepath where the readings
 % are.
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-FILEPATH = "../../python_scripts/test_bias12.csv";
-readings = readtable(FILEPATH, 'VariableNamingRule', 'preserve');
-y_data = readings.topMid';
-=======
 FILEPATH = "../../python_scripts/test1.csv";
-=======
-FILEPATH = "../../python_scripts/test2.csv";
->>>>>>> 564e173 (Massive changes to code, and perhaps a new success???)
-=======
-FILEPATH = "../../python_scripts/test1.csv";
->>>>>>> cbd11b7 (good parameters)
-=======
-FILEPATH = "../../python_scripts/test3.csv";
->>>>>>> 61698af (Circuit and PCB)
-=======
-FILEPATH = "../../python_scripts/Estimate_R.csv";
->>>>>>> 63c81e0 (estimate of sensor meassurment)
-=======
-FILEPATH = "../../python_scripts/test6.csv";
->>>>>>> d0ba3a8 (estimate R changes)
-=======
-FILEPATH = "../../python_scripts/test6.csv";
->>>>>>> 4f56570 (Changes in the new estimator, will try to finalize tomorrow)
-=======
-FILEPATH = "../../python_scripts/test1.csv";
->>>>>>> beb4004 (Full Kalman Filter implementation in MATLAB)
-=======
-FILEPATH = "../../python_scripts/test1.csv";
->>>>>>> e10bbb0 (New parameter estimation changes)
-=======
-FILEPATH = "../../python_scripts/test_movavg_10ms.csv";
->>>>>>> a7ffe45 (New changes to parameter estimation)
-=======
-FILEPATH = "../../python_scripts/test_movavg_10ms5.csv";
->>>>>>> 9b8dcd8 (bigger movavg)
-=======
-FILEPATH = "../../python_scripts/test_movavg_10ms6.csv";
->>>>>>> 6ff7809 (functions for arduino code)
-=======
-FILEPATH = "../../python_scripts/test1.csv";
->>>>>>> a3698cc (Last changes to the files)
 readings = readtable(FILEPATH, 'VariableNamingRule', 'preserve');
 y_data = readings.Data1';
->>>>>>> 3c45509 (Opened a new branch for parameter estimation. Purpose of this branch is to fix the estimation algorithm)
-N = max(size(y_data));      %Number of samples to be registered
-<<<<<<< HEAD
-tspan = 180;                %Time span of the simulation in seconds
-=======
-FILEPATH = "../../python_scripts/test_bias2.csv";
-=======
-FILEPATH = "../../python_scripts/Test_movavg_10ms6.csv";
->>>>>>> 4eaf0d9 (Plots for the report, as well as a new constraint calculation)
-readings = readtable(FILEPATH, 'VariableNamingRule', 'preserve');
-y_data = readings.Data1';
-N = max(size(y_data));      %Number of samples to be registered
-<<<<<<< HEAD
-tspan = 60;                %Time span of the simulation in seconds
->>>>>>> dc87003 (test bias1 and 2)
-=======
-tspan = 300;                 %Time span of the simulation in seconds
->>>>>>> 4eaf0d9 (Plots for the report, as well as a new constraint calculation)
-M_size = 100;
-=======
-tspan = 240;                %Time span of the simulation in seconds
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 564e173 (Massive changes to code, and perhaps a new success???)
-=======
-M_size = 50;
->>>>>>> e10bbb0 (New parameter estimation changes)
-=======
-M_size = 100;
->>>>>>> a7ffe45 (New changes to parameter estimation)
-=======
-FILEPATH = "../../python_scripts/Test_movavg_10ms6.csv";
-readings = readtable(FILEPATH, 'VariableNamingRule', 'preserve');
-y_data = readings.Data1';
-N = max(size(y_data));      %Number of samples to be registered
-=======
-FILEPATH = "../../python_scripts/test_bias8.csv";
-readings = readtable(FILEPATH, 'VariableNamingRule', 'preserve');
-y_data = readings.Var1';
-N = max(size(y_data));      %Number of samples to be registered
->>>>>>> master
 tspan = 360;                %Time span of the simulation in seconds
 M_size = 100;
 
@@ -116,63 +18,17 @@ M_size = 100;
 %         y_data(i) = 0.0;
 %     end
 % end
-<<<<<<< HEAD
->>>>>>> 2393880 (Final updates to the code)
-=======
->>>>>>> master
+
 %% INITIALIZING SIMULATION
 t_vec = linspace(0, tspan, N); %Time vector for plotting and input generation
 
 % Defining the phi parameters. These parameters are defined as the theta
 % parameters adjusted for the sample time. These parameters must be within
 % 0 and 1. The last parameter is the total muscle mass. This parameter does
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-% not need to be adjusted for the sample time, but will be included as a
-% family of different parameters.
-phi_first_guess = [0.9, 0.1, 0.1, 0.9]; 
-M = linspace(3, 40, M_size);
-=======
-% not need to be adjusted for the sample time.
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-phi_first_guess = [0.3, 0.7, 0.9, 0.3, 20];
->>>>>>> 3c45509 (Opened a new branch for parameter estimation. Purpose of this branch is to fix the estimation algorithm)
-=======
-phi_first_guess = [0.9, 0.7, 0.6, 0.9, 20];
->>>>>>> cbd11b7 (good parameters)
-=======
-phi_first_guess = [0.99, 0.7, 0.6, 0.9, 20]; 
-=======
-phi_first_guess = [0.99, 0.7, 0.6, 0.9]; 
-
-M = linspace(0,30,1000);
->>>>>>> 6d69de0 (M - changes)
-=======
-% not need to be adjusted for the sample time, but will be included as a
-% family of different parameters.
-<<<<<<< HEAD
-phi_first_guess = [0.2, 0.5, 0.9, 0.7]; 
-<<<<<<< HEAD
-M = linspace(3, 30, 100);
->>>>>>> e10bbb0 (New parameter estimation changes)
-=======
-M = linspace(3, 50, 1000);
->>>>>>> cd85389 (git playing games)
-% af, 
->>>>>>> 61698af (Circuit and PCB)
-=======
-phi_first_guess = [0.5, 0.5, 0.5, 0.5]; 
-M = linspace(3, 40, M_size);
->>>>>>> a7ffe45 (New changes to parameter estimation)
-=======
 % not need to be adjusted for the sample time, but will be included as a
 % family of different parameters.
 phi_first_guess = [0.5, 0.5, 0.5, 0.5]; 
 M = linspace(3, 40, M_size);
->>>>>>> master
 
 % The input signal is defined below. The function is then run with each
 % element.
@@ -190,39 +46,13 @@ mk = NaN(2, N, M_size);
 mk(:, 1, :) = zeros(2, 1, M_size);
 
 % Running simulation
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> master
 for j = 1:M_size
     for i = 2:N
         mk(:, i, j) = disc_diff_eq(phi_first_guess, mk(:, i-1, j), u_vec(i-1), M(j));
-    end
-<<<<<<< HEAD
-=======
-for i = 2:N
-<<<<<<< HEAD
-    mk(:, i) = disc_diff_eq(phi_first_guess, mk(:, i-1), u_vec(i-1));
->>>>>>> 4f56570 (Changes in the new estimator, will try to finalize tomorrow)
-=======
-    for j = 1:1000
-        mk(:, i) = disc_diff_eq(phi_first_guess, mk(:, i-1), u_vec(i-1),M(j));
-=======
-for j = 1:M_size
-    for i = 2:N
-        mk(:, i, j) = disc_diff_eq(phi_first_guess, mk(:, i-1, j), u_vec(i-1), M(j));
->>>>>>> e10bbb0 (New parameter estimation changes)
-    end
->>>>>>> 6d69de0 (M - changes)
-end
-
-=======
 end
 
 % Splitting the results
 
->>>>>>> master
 % % Plotting the results
 % for i = 1:M_size
 %     figure(1)
@@ -245,108 +75,6 @@ end
 % parameters
 type disc_theta_to_ode
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-phi = optimvar('phi', 4);
-=======
-phi = optimvar('phi', 4, 'LowerBound', [0, 0, 0, 0], 'UpperBound', [1, 1, 1, 1]);
->>>>>>> a7ffe45 (New changes to parameter estimation)
-
-% Now, we express this function as an optimization expression.
-optim_y = optimexpr(1, N);
-sumsq = NaN(1, M_size);
-phi_estims = NaN(4, M_size);
-
-for i = 1:M_size
-    fprintf("Problem %i", i);
-    fcn = fcn2optimexpr(@disc_theta_to_ode, phi, N, u_vec, M(i));
-    optim_y = fcn;
-    % Finally, the objective function can be defined.
-    obj = sum((y_data - optim_y).^2);
-    % Now, the optimization problem
-    opts = optimoptions(@fmincon, "MaxFunEvals", 9e3);
-    prob = optimproblem("Objective", obj);
-    prob.Constraints.cons1 = -phi(1) + phi(4) + phi(2) + (M(i)*phi(3)) >= 0.0001;
-    prob.Constraints.cons2 = phi(1) + phi(4) - (2*phi(1)*phi(4)) + (phi(4)*phi(2)) + (M(i)*phi(4)*phi(3)) >= 1.0001;
-    prob.Constraints.cons3 = -phi(1) + phi(4) + phi(3) + (M(i)*phi(3)) >= 0.0001;
-    prob.Constraints.cons4 = phi(1) + phi(4) - (2*phi(1)*phi(4)) - (phi(1)*phi(3)) + (phi(4)*phi(3)) + (M(i)*phi(4)*phi(3)) >= 1.0001;
-    % Initial guess on theta
-    phi_0.phi = phi_first_guess;
-    % Solve the optimization problem
-    [phi_sol, sumsq(i)] = solve(prob, phi_0, 'Options', opts);
-    phi_estims(:, i) = phi_sol.phi;
-=======
-phi = optimvar('phi', 5);
-=======
-phi = optimvar('phi', 4);
->>>>>>> 6d69de0 (M - changes)
-
-% Now, we express this function as an optimization expression.
-%fcnt = @(theta) theta_to_ode(theta, tspan, m0, u);
-=======
-phi = optimvar('phi', 4, 'LowerBound', [0, 0, 0, 0]);
-
-% Now, we express this function as an optimization expression.
->>>>>>> cd85389 (git playing games)
-optim_y = optimexpr(1, N);
-sumsq = NaN(1, M_size);
-phi_estims = NaN(4, M_size);
-
-<<<<<<< HEAD
-% Finally, the objective function can be defined.
-obj = sum((y_data - optim_y).^2);
-
-% Now, the optimization problem
-prob = optimproblem("Objective", obj);
-%% OPTIMIZATION PROBLEM: SOLVE
-% Initial guess on theta
-phi_0.phi = phi_first_guess;
-
-% Solve the optimization problem
-[phi_sol, sumsq] = solve(prob, phi_0);
-
-disp(phi_sol.phi)
-disp(sumsq)
-%% PLOT ALL RESULTS
-m_est = NaN(2, N);
-m_est(:, 1) = zeros(2, 1);
-for i = 2:N
-<<<<<<< HEAD
-<<<<<<< HEAD
-    m_est(:, i) = disc_diff_eq(m_est(:, i-1), phi_sol.phi, u_vec(i));
->>>>>>> 3c45509 (Opened a new branch for parameter estimation. Purpose of this branch is to fix the estimation algorithm)
-=======
-    m_est(:, i) = disc_diff_eq(phi_sol.phi, m_est(:, i-1), u_vec(i));
->>>>>>> 4f56570 (Changes in the new estimator, will try to finalize tomorrow)
-=======
-    m_est(:, i) = disc_diff_eq(phi_sol.phi, m_est(:, i-1), u_vec(i), M);
->>>>>>> 6d69de0 (M - changes)
-=======
-for i = 1:M_size
-    fcn = fcn2optimexpr(@disc_theta_to_ode, phi, N, u_vec, M(i));
-    optim_y = fcn(1, :);
-    % Finally, the objective function can be defined.
-    obj = sum((y_data - optim_y).^2);
-    % Now, the optimization problem
-    prob = optimproblem("Objective", obj);
-    prob.Constraints.cons1 = phi(4) + phi(2) - phi(1) + (M(i)*phi(3)) >= 0.00001;
-    prob.Constraints.cons2 = (M(i)*phi(3)*phi(4)) + phi(1) + phi(4) - (2*phi(1)*phi(4)) + (phi(4)*phi(2)) >= 1.00001;
-    prob.Constraints.cons3 = phi(3) + phi(4) - phi(1) + (M(i)*phi(3)) >= 0.00001;
-    prob.Constraints.cons4 = (M(i)*phi(3)*phi(4)) + phi(1) + phi(4) + phi(3) - (2*phi(1)*phi(4)) - (phi(1)*phi(3)) + (phi(4)*phi(3)) >= 1.00001;
-    % Initial guess on theta
-    phi_0.phi = phi_first_guess;
-    % Solve the optimization problem
-    [phi_sol, sumsq(i)] = solve(prob, phi_0);
-<<<<<<< HEAD
-    phi_estims(i, :) = phi_sol.phi;
->>>>>>> e10bbb0 (New parameter estimation changes)
-=======
-    phi_estims(:, i) = phi_sol.phi;
->>>>>>> cd85389 (git playing games)
-=======
 phi = optimvar('phi', 4);
 
 % Now, we express this function as an optimization expression.
@@ -372,7 +100,6 @@ for i = 1:M_size
     % Solve the optimization problem
     [phi_sol, sumsq(i)] = solve(prob, phi_0, 'Options', opts);
     phi_estims(:, i) = phi_sol.phi;
->>>>>>> master
 end
 
 [min, min_index] = min(sumsq);
@@ -382,71 +109,12 @@ m_est = NaN(2, N, M_size);
 m_est(:, 1, :) = zeros(2, 1, M_size);
 for j = 1:M_size
     for i = 2:N
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         m_est(:, i, j) = disc_diff_eq(phi_estims(:, j), m_est(:, i-1, j), u_vec(i), M(j));
-=======
-        m_est(:, i, j) = disc_diff_eq(phi_estims(j, :), m_est(:, i-1), u_vec(i), M(j));
->>>>>>> e10bbb0 (New parameter estimation changes)
-=======
-        m_est(:, i, j) = disc_diff_eq(phi_estims(:, j), m_est(:, i-1, j), u_vec(i), M(j));
->>>>>>> cd85389 (git playing games)
-=======
-        m_est(:, i, j) = disc_diff_eq(phi_estims(:, j), m_est(:, i-1, j), u_vec(i), M(j));
->>>>>>> master
     end
 end
 
 figure(1)
 hold on
-<<<<<<< HEAD
-plot(t_vec, mk(1, :, min_index), '--');
-plot(t_vec, mk(2, :, min_index), '--');
-plot(t_vec, m_est(1, :, min_index));
-plot(t_vec, m_est(2, :, min_index));
-plot(t_vec, y_data);
-plot(t_vec, u_vec);
-legend("Active Muscle Mass", "Fatigued Muscle Mass", "Estimated Active Muscle Mass", "Estimated Fatigued Muscle Mass");
-<<<<<<< HEAD
-<<<<<<< HEAD
-xlabel("Time (s)")
-ylabel("Mass (kg)")
-title("Hand Grip System Identification")
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-xlabel("Time (s)", "FontSize", 14)
-ylabel("Mass (kg)", "FontSize", 14)
-title("Hand Grip System Identification: Test 12m", "fontSize", 22)
->>>>>>> 2393880 (Final updates to the code)
-=======
-xlabel("Time (s)")
-ylabel("Mass (kg)")
-title("Hand Grip System Identification: Test 12", "fontSize", 34)
->>>>>>> 39930d4 (Last commit?)
-hold off
-disp(phi_estims(:, min_index))
-<<<<<<< HEAD
-<<<<<<< HEAD
-disp(M(min_index))
-disp(sumsq(min_index))
-=======
-hold off
->>>>>>> e10bbb0 (New parameter estimation changes)
-=======
-hold off
-
-disp(phi_estims(:, min_index))
->>>>>>> cd85389 (git playing games)
-=======
-disp(M(min_index))
->>>>>>> a3698cc (Last changes to the files)
-=======
-disp(M(min_index))
-disp(min);
->>>>>>> 4eaf0d9 (Plots for the report, as well as a new constraint calculation)
-=======
 plot(t_vec, mk(1, :, min_index), '--','LineWidth',7);
 plot(t_vec, mk(2, :, min_index), '--','LineWidth',7);
 plot(t_vec, m_est(1, :, min_index),'LineWidth',7);
@@ -462,4 +130,3 @@ hold off
 disp(phi_estims(:, min_index))
 disp(M(min_index))
 disp(sumsq(min_index))
->>>>>>> master
