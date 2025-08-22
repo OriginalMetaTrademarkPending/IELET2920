f9efd66fedc0a243f8958acbfb1167c25fa802f1
OriginalMetaTrademarkPending <metaadmir@gmail.com>
2025-08-22 11:39:20 +0200

    Reverted the bachelor_esp_kalman file to functioning (?) version.

--- bachelor_esp_kalman/src/main.cpp
+++ bachelor_esp_kalman/src/main.cpp
@@ -6,18 +6,12 @@ using namespace BLA;
 
 // screen setup and naming
 TFT_eSPI tft = TFT_eSPI();
-<<<<<<< HEAD
-
-
-const long sampletime = 5; 
-=======
 
 float prev = 0;
 float now = 0;
 
 
 const long sampletime = 10; 
->>>>>>> master
 const long samplePrintTime = 10;
 
 struct KalmanFilter
@@ -58,37 +52,23 @@ struct KalmanFilter
   BLA::Matrix<2, 1> B = {phi_ra * M, 0};
   const BLA::Matrix<2, 2> I = {1., 0., 0., 1.}; 
 
-<<<<<<< HEAD
-=======
   float flagDer = 0.;
 
->>>>>>> master
   void read()
   {
     z_prev = z;
     reading = analogRead(pin);
     z(0) = {static_cast<float>(13.8384*reading)/4095};
-<<<<<<< HEAD
-=======
     flagDer = (z(0) - z_prev(0))/sampletime;
->>>>>>> master
   }
 
   void predict()
   { 
-<<<<<<< HEAD
-    bool flagDer = (z(0) - z_prev(0))/sampletime > -10;
-    bool flagLow = z(0) > 0.1;
-    float last_pred = 0.;
-
-    if (flagDer and flagLow)
-=======
     //bool flagDer = (z(0) - z_prev(0))/sampletime > -10;
     bool flagLow = z(0) > 0.1;
     float last_pred = 0.;
 
     if (flagDer > -0.04 and flagLow)
->>>>>>> master
     {
       F = {phi_af - phi_ra, 1 - phi_fa - phi_ra,
           1 - phi_af, phi_fa};
@@ -103,12 +83,6 @@ struct KalmanFilter
       last_pred = prediction.x(1);
       prediction.x = (F * estimate.x);
       
-<<<<<<< HEAD
-      if (last_pred < prediction.x(1))  {
-        prediction.x(1) = last_pred;
-      }
-=======
->>>>>>> master
       
     }
     prediction.P = (F * estimate.P * ~F) + (Q);
@@ -123,7 +97,7 @@ struct KalmanFilter
     S = (H * prediction.P * ~H) + R;
 
     // Calculate the kalman gain
-    K = (prediction.P * ~H) * Invert(S);
+    K = (prediction.P * ~H) * Inverse(S);
 
     // Calculate the estimates and state covariance matrix
     estimate.x = prediction.x + (K * y);
@@ -145,28 +119,7 @@ int topMidPixels[2];
 int botMidPixels[2];
 int botPixels[2];
 
-<<<<<<< HEAD
-
-
-// make true to display the raw sensordata in red
-const bool displayRaw = true;
-
-// make false to not display the filtered sensors
-const bool displayFiltered = true;
-
-// make true/false to enable/disable sensors
-const bool topsens = true;
-
-// timeline of measurement for display
-int topPixels[4];
-int topMidPixels[4];
-int botMidPixels[4];
-int botPixels[4];
-
-//  current graphing pixel
-=======
 // Current graphing pixel
->>>>>>> master
 int i = 0;
 
 // Scales of display
@@ -178,14 +131,9 @@ int button1Pin = 0;
 int button2Pin = 35;
 
 
-<<<<<<< HEAD
-void topDisplay();
-
-=======
 void Display();
 
 // initialize sensors
->>>>>>> master
 KalmanFilter topSensor;
 KalmanFilter topMidSensor;
 KalmanFilter botMidSensor;
@@ -193,10 +141,6 @@ KalmanFilter botSensor;
 
 void setup()
 {
-<<<<<<< HEAD
-  // put your setup code here, to run once:
-=======
->>>>>>> master
   // Screen startup
   tft.init();
   tft.setRotation(3);
@@ -238,14 +182,8 @@ void loop()
   }
   
   /*
-<<<<<<< HEAD
-  // print for datacollection
-  if((millis() - sampleStartTime) >= samplePrintTime){
-    //static_cast<float>
-=======
   // print for datacollection to be read by computer
   if((millis() - sampleStartTime) >= samplePrintTime){
->>>>>>> master
     Serial.print(String(static_cast<float>(topSensor.estimate.x(0))));
     Serial.print(",");
     Serial.print(String(static_cast<float>(topMidSensor.estimate.x(0))));
@@ -259,22 +197,6 @@ void loop()
   */
 
 
-<<<<<<< HEAD
-
-
-  if (millis() - prevprint >= 20) {
-    if (picker == 0 or picker == 3)  {
-      Serial.print("x musselmasstop: "); Serial.print(topSensor.estimate.x(1)); Serial.print("  ");
-      Serial.print("x est: "); Serial.print(topSensor.estimate.x(0)); Serial.print("  ");
-      Serial.print("z: "); Serial.print(topSensor.z(0)); Serial.print("  ");
-    }
-    if (picker == 1 or picker == 3)  {
-      Serial.print("y: "); Serial.print(topSensor.y(0)); Serial.print("  ");
-      Serial.print("P: "); Serial.print(sqrt(topSensor.estimate.P(0))*3); Serial.print("  ");
-      Serial.print("P neg: "); Serial.print(-sqrt(topSensor.estimate.P(0))*3); Serial.print("  ");
-    }
-    Serial.println("uT");
-=======
   // print for Serial plotter debugging
   
   if (millis() - prevprint >= 20) {
@@ -289,21 +211,12 @@ void loop()
       Serial.print("-3_sigma: "); Serial.print(-sqrt(topSensor.estimate.P(0))*3); Serial.print("  ");
     }
     Serial.println("uT"); // DO NOT REMOVE - will not serial plot without this
->>>>>>> master
     
     prevprint = millis();
   }
   
-<<<<<<< HEAD
- 
-  if (digitalRead(button1Pin) == 0) {
-    topSensor.estimate.x(1) = 0;
-    topSensor.prediction.x(1) = 0;
-  }
-=======
   
 
->>>>>>> master
 
   // grafing
   // change display scale and reset display
@@ -325,26 +238,6 @@ void loop()
       tft.fillScreen(TFT_BLACK);
     }
 
-<<<<<<< HEAD
-  if (millis() - prevprint >= 20) {
-    // scrolling thru screen
-    i++;
-    if (i > 241)  {
-      i = 0;
-      tft.fillScreen(TFT_BLACK);
-    }
-
-
-    // display the picked scale and the raw and filtered bottomsensor in numbers
-    tft.drawString("Scale: " + String(scales[picker]),155,0);
-
-    // top sensor
-    
-    if (topsens)  {
-      topDisplay();
-    }
-    prevprint = millis();
-=======
     // display the picked scale and the raw and filtered bottomsensor in numbers
     tft.drawString("Scale: " + String(scales[picker]),155,0);
     float prev = now;
@@ -353,7 +246,6 @@ void loop()
     tft.drawString("Derivative: " + String(topSensor.flagDer),130,20);
     Display();
     prevscreenprint = millis();
->>>>>>> master
   }
 
   
@@ -361,28 +253,6 @@ void loop()
   
 }
 
-<<<<<<< HEAD
-
-void topDisplay()
-{
-  if (displayFiltered)
-  {
-    topPixels[1] = topPixels[0];
-    topPixels[0] = map((4095*topSensor.estimate.x(0))/13.8384, 0, scales[picker], 130, 1);
-    tft.drawLine(i, topPixels[0], i, topPixels[1], TFT_BLUE);
-    topMidPixels[1] = topMidPixels[0];
-    topMidPixels[0] = map((4095*topMidSensor.estimate.x(0))/13.8384, 0, scales[picker], 130, 1);
-    tft.drawLine(i, topMidPixels[0], i, topMidPixels[1], TFT_GREENYELLOW);
-    botMidPixels[1] = botMidPixels[0];
-    botMidPixels[0] = map((4095*botMidSensor.estimate.x(0))/13.8384, 0, scales[picker], 130, 1);
-    tft.drawLine(i, botMidPixels[0], i, botMidPixels[1], TFT_ORANGE);
-    botPixels[1] = botPixels[0];
-    botPixels[0] = map((4095*botSensor.estimate.x(0))/13.8384, 0, scales[picker], 130, 1);
-    tft.drawLine(i, botPixels[0], i, botPixels[1], TFT_RED);
-  }
-}
-
-=======
 
 void Display()
 {
@@ -404,4 +274,3 @@ void Display()
   tft.drawLine(i, botPixels[0], i, botPixels[1], TFT_RED);
 }
 
->>>>>>> master
